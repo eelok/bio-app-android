f23f7c9342983d81de6b637df4307ad231629894
eelok <eelok@mail.ru>
2021-09-05 20:40:10 +0200

    create Bio and  bind it

--- app/src/main/java/com/eelok/bio/MainActivity.java
+++ app/src/main/java/com/eelok/bio/MainActivity.java
@@ -8,25 +8,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.eelok.bio.data.Bio;
 import com.eelok.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private final Bio bio = new Bio();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.invalidateAll();
+        bio.setName("Eelok Eelok");
+        binding.setBio(bio);
         binding.doneButton.setOnClickListener(view -> handleDoneBtn(view));
     }
 
 
     public void handleDoneBtn(View view) {
-        binding.hobbiesOutput.setText(String.format("Hobbies: %s",  binding.enterHobbies.getText()));
+        bio.setHobbies(String.format("Hobbies: %s", binding.enterHobbies.getText().toString().trim()));
+
+        binding.invalidateAll();
         binding.hobbiesOutput.setVisibility(View.VISIBLE);
+
 
         //hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
